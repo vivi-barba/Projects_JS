ffba7690f24e485f774fbb88c89441cec2a0803f
vivi-barba <vitoriabbarbosa31@gmail.com>
2023-03-03 18:56:10 -0300

    JS-projects

--- estudando cs/TiaIdentity/exemplo/Models/Constantes/Perfis.cs
+++
@@ -1,8 +0,0 @@
-namespace App.Models
-{
-    public static class Perfis
-    {
-        public const string Administrador = "Administrador";        
-        public const string Comum = "Comum";        
-    }
-}
